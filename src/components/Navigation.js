c2f65e185c6cdafd3545b1f2995090db1a666cab
Aaron Moreno <aaron.j.moreno@gmail.com>
2020-12-19 23:56:03 -0700

    added media queries

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,6 +6,7 @@ const Navigation = ({}) => {
 
     return(<nav className="container grid grid-cols-3 min-w-full p-0 m-0">
                 <img src={logo} className="w-20 col-span-2" style={{width: '80px'}} lazyload="true"/>
+
                 <ul className="pt-7 text-center w-auto">
                     <Link to="/" className='mx-2'>home</Link>
                     <Link to="/#about" className='mx-2'>about</Link>
