815905afa8e42bf583cb32e39686723deb01b292
Aaron Moreno <aaron.j.moreno@gmail.com>
2020-12-21 14:34:27 -0700

    fixed form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,8 +10,9 @@ const Contact = () => {
                 name="contact"
                 method="POST"
                 data-netlify="true"
+                data-netlify-honeypot="bot-field"
             >
-                <input type="hidden" name="contact" value="contact" />
+                <input type="hidden" name="form-name" value="contact" />
 
                 <input type="text" name="name" placeholder="[name]"/>
 
